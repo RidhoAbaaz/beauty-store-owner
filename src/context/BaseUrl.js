97def3ff55548ee8a224917344af9bddec55d7b7
ridhoabaaz <aaridho16@gmail.com>
2026-03-25 14:56:45 +0700

    update backend url

--- src/context/BaseUrl.js
+++ src/context/BaseUrl.js
@@ -1,3 +1,3 @@
 import { createContext } from "react";
 
-export const BaseUrl = createContext("http://localhost:5000")
+export const BaseUrl = createContext("https://backend-service-450920673819.asia-southeast2.run.app");
